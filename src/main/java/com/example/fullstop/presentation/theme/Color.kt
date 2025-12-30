7dd8844153869d9cfff434847d74fc5b14543d5a
j0chan <wdc4512@naver.com>
2025-12-30 12:46:54 +0900

    chore: 프로젝트 초기 구조 설정 및 라이브러리 의존성 추가

--- src/main/java/com/example/fullstop/presentation/theme/Color.kt
+++ src/main/java/com/example/fullstop/presentation/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.fullstop.ui.theme
+package com.example.fullstop.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
--- src/main/java/com/example/fullstop/presentation/theme/Type.kt
+++ src/main/java/com/example/fullstop/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.fullstop.ui.theme
+package com.example.fullstop.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
